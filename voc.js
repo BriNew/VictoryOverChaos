e7491c1ca3084aad3b1c379c64f5a539ee9f772c
Brian Newell <briannewell@fastmail.com>
2021-01-09 17:00:19 -0600

    js nav

--- voc.js
+++ voc.js
@@ -1,1 +1,11 @@
-//js
+$(document).ready(function() {
+  $(".home_link, .about_link, .services_link, .quotes_link").on("click", function( e ) {
+
+    e.preventDefault();
+    console.log($(this).attr('href'));
+    $("body, html").animate({ 
+      scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 400);
+
+  });
+});
